3447672ba3de3d204e0b1ea13e353e1de70c940d
KRW <classic.krw@gmail.com>
2023-02-26 03:19:13 +0100

    0.0.7 ig

--- installer.bat
+++ installer.bat
@@ -6,7 +6,7 @@ rem "cls" here clears output of chcp command that says its now using other chara
 cls
 
 rem Version variables
-set "ver=a0.0.6"
+set "ver=a0.0.7"
 set "OMEm-ver-validator=1"& rem check "Module Version Override.txt" file
 
 rem Set "select" variable to "r" to make sure installer.bat doesnt crash when nothing is inputted on fresh boot
